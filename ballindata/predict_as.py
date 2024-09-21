d79b0343a6100e2ae43587362cb2f0af1f6fced4
Brandon Chan <bchany100@gmail.com>
2024-09-21 09:14:10 -0400

    Updated to latest no tensorflow version

--- ballindata/predict_as.py
+++ ballindata/predict_as.py
@@ -2,7 +2,7 @@ import pandas as pd, numpy as np, sklearn as sklm
 import tensorflow as tf, sqlalchemy 
 from sklearn.linear_model import LogisticRegression, LinearRegression 
 import joblib 
-from keras.models import load_model 
+# from keras.models import load_model 
 import os 
 from django.conf import settings 
 
@@ -13,15 +13,15 @@ master = pd.read_sql("master_as", con=engine)
 numeric_df = pd.read_sql("numeric_as", con=engine)  
 rf = joblib.load(os.path.join(settings.BASE_DIR, 'ballindata/IPYNB/Analyses/rf_model.pkl')) 
 lr = joblib.load(os.path.join(settings.BASE_DIR, 'ballindata/IPYNB/Analyses/lr_model.pkl')) 
-nn = load_model(os.path.join(settings.BASE_DIR, 'ballindata/IPYNB/Analyses/seq_model.keras')) 
+# nn = load_model(os.path.join(settings.BASE_DIR, 'ballindata/IPYNB/Analyses/seq_model.keras')) 
 
 def make_prediction(data, selected_model):
     if selected_model == 'logistic_regression': 
         prediction = lr.predict(data)[0] 
     elif selected_model == 'random_forest': 
         prediction = rf.predict(data)[0] 
-    else: 
-        prediction = nn.predict(tf.convert_to_tensor(data))[0][0].astype('float64') 
+    # else: 
+    #     prediction = nn.predict(tf.convert_to_tensor(data))[0][0].astype('float64') 
     
     
     # if isinstance(prediction, np.ndarray):
