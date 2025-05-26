fc1eee99eadb1d8817e7eed53d2d4458ef51d9ea
Brandon Chan <bchany100@gmail.com>
2025-05-26 02:23:20 +0000

    Working dockerized website

--- ballindata/predict_as.py
+++ ballindata/predict_as.py
@@ -1,20 +1,20 @@
 import pandas as pd, numpy as np, sklearn as sklm
-import sqlalchemy 
+import sqlalchemy
 from sklearn.linear_model import LogisticRegression, LinearRegression 
-import joblib 
-import tensorflow as tf
-import keras 
-from keras.models import load_model 
+import joblib
+#import tensorflow as tf
+#import keras
+#from keras.models import load_model
 import os 
 from django.conf import settings 
 
 def make_prediction(names, data, selected_model): 
     stat_string = '_'.join(names) 
     
-    if selected_model == 'neural_network':
-        model = load_model(os.path.join(settings.BASE_DIR, f'ballindata/MLMODELS/allstar/nn/{stat_string}.keras')) 
-        prediction = model.predict(tf.convert_to_tensor(data))[0][0].astype('float64')         
-    elif selected_model == 'logistic_regression': 
+   # if selected_model == 'neural_network':
+    #    model = load_model(os.path.join(settings.BASE_DIR, f'ballindata/MLMODELS/allstar/nn/{stat_string}.keras')) 
+     #   prediction = model.predict(tf.convert_to_tensor(data))[0][0].astype('float64')         
+    if selected_model == 'logistic_regression': 
         model = joblib.load(os.path.join(settings.BASE_DIR, f'ballindata/MLMODELS/allstar/lr/{stat_string}.pkl'))
         prediction = model.predict_proba(data)[0][1] 
     elif selected_model == 'random_forest': 
